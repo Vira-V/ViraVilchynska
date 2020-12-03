490da5077527a2464cb6b2094f7f4cb3e4346495
Vira <vira.vilchinska.dk@gmail.com>
2020-12-03 18:43:23 +0200

    home22w

--- Homework/project HW20/js/customs.js
+++ Homework/project HW20/js/customs.js
@@ -49,4 +49,49 @@ jQuery(document).ready(function () {
 		}
 	});
 
+	var slid_client = jQuery('#client-slider');
+		
+	slid_client.owlCarousel({
+		items: 4,
+		autoplay: false,
+		autoplayTimeout: 6000,
+		autoplayHoverPause: false,
+		smartSpeed: 1000,
+		dotsSpeed: 1000,
+		loop: true,
+		nav: true,
+		navText: ['', ''],
+		dots: false,
+		mouseDrag: true,
+		margin: 30,
+		stagePadding: 0,
+		autoWidth: false,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 1,
+				dots: true,
+				nav: false,
+				mouseDrag: true,
+				autoplay: false
+			},
+			479:{
+				items: 1,
+				mouseDrag: true
+			},
+			767:{
+				items: 1,
+				mouseDrag: true
+			},
+			991:{
+				items: 1,
+				mouseDrag: true
+			},
+			1099:{
+				items: 4,
+				mouseDrag: true
+			}
+		}
+	});
+
 	});
